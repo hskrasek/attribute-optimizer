64d0522f1036c60100f5fe286967fb29d9557048
Hunter Skrasek <hunterskrasek@me.com>
2023-11-25 02:14:47 -0600

    Improve skill queue command
    Signed-off-by: Hunter Skrasek <hunterskrasek@me.com>

--- app/Commands/Skills/Queue.php
+++ app/Commands/Skills/Queue.php
@@ -5,7 +5,11 @@ namespace App\Commands\Skills;
 use App\ESI\Auth\Token;
 use App\ESI\Http\Middleware;
 use App\ESI\SDE;
+use App\Type;
+use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
 use LaravelZero\Framework\Commands\Command;
@@ -45,20 +49,25 @@ class Queue extends Command
             //TODO: Better
             'User-Agent' => 'Eve-Online-CLI-App',
             'Authorization' => 'Bearer ' . $token->accessToken,
-        ])->withMiddleware(Middleware::refreshToken($token))->get('https://esi.evetech.net/latest/characters/' . $token->characterId() . '/skillqueue')
+        ])->withMiddleware(Middleware::refreshToken($token))->get(
+            'https://esi.evetech.net/latest/characters/' . $token->characterId() . '/skillqueue'
+        )
             ->throw()
             ->json();
 
-        //TODO: Look into use Termwind for this
+        /** @var Collection<string, Type> $skills */
+        $skills = Type::with('group')
+            ->whereIn('typeID', Arr::pluck($skillQueue, 'skill_id'))
+            ->get()
+            ->keyBy('typeID');
+
         $this->table(
-            ['Skill', 'Level', 'Start Time', 'Finish Time', 'Training Start SP', 'Training Destination SP',],
-            array_map(fn (array $skill) => [
-                SDE::types($skill['skill_id'])['name']['en'],
+            ['Group', 'Skill', 'Level', 'Finishes In',],
+            array_map(fn(array $skill) => [
+                $skills->get($skill['skill_id'])->group->groupName,
+                $skills->get($skill['skill_id'])->typeName,
                 $skill['finished_level'],
-                $skill['start_date'],
-                $skill['finish_date'],
-                $skill['training_start_sp'],
-                $skill['level_end_sp'],
+                Carbon::parse($skill['finish_date'])->diffForHumans(),
             ], $skillQueue)
         );
 
@@ -68,7 +77,7 @@ class Queue extends Command
     /**
      * Define the command's schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     public function schedule(Schedule $schedule): void
